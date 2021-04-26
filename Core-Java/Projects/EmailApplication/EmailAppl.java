e89c87f05f1c26d2a9b980e1cb83f5fb76bc85a3
Krithick S <67591293+Krith-10@users.noreply.github.com>
2021-04-26 16:07:30 +0530

    Add files via upload

---
+++ Core-Java/Projects/EmailApplication/EmailAppl.java
@@ -0,0 +1,11 @@
+package MiniCapstone.EmailApplication;
+
+public class EmailAppl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EmailGen eg = new EmailGen("Aaab","R");
+		System.out.println(eg.showInfo());
+	}
+
+}
